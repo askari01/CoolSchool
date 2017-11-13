dbc66143a0850e763719f89b5211664a133b6024
askari01 <farrukh.askari01@gmail.com>
2017-11-13 14:24:13 +0500

    added button to buy with functionality on exact cell in table view cell

--- CoolSchool/VideosCVCell.swift
+++ CoolSchool/VideosCVCell.swift
@@ -9,15 +9,35 @@
 import UIKit
 import Kingfisher
 
+// For Button Action
+protocol CellDelegate : class {
+    func didPressButton(_ tag: Int)
+}
+
 class VideosCVCell: UICollectionViewCell {
     
+    weak var cellDelegate: CellDelegate?
+    
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var viualEffect: UIVisualEffectView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        buyButton.layer.cornerRadius = 5
     }
     
-    func setValues(picture1: URL ) {
+    @IBAction func buyAction(_ sender: UIButton) {
+        cellDelegate?.didPressButton(self.tag)
+        viualEffect.isHidden = true
+        buyButton.isHidden = true
+    }
+    
+    func setValues(picture1: URL, row: Int ) {
+        if row == 0 {
+            viualEffect.isHidden = true
+            buyButton.isHidden = true
+        }
         self.image.kf.indicatorType = .activity
         self.image.kf.setImage(with: picture1)
     }
--- CoolSchool/ViewController.swift
+++ CoolSchool/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, CellDelegate {
 
+    let videoData = [("First Video", "8ueYlFT0G0c/0"), ("Second Video","kQkzHhFC0cc/0"), ("Third Video", "DEVVdu2jSlo/0"), ("First Video", "8ueYlFT0G0c/0"), ("Second Video","kQkzHhFC0cc/0"), ("Third Video", "DEVVdu2jSlo/0")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -36,16 +38,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! VideosCVCell
-        let videoData = [("First Video", "8ueYlFT0G0c/0"), ("Second Video","kQkzHhFC0cc/0"), ("Third Video", "DEVVdu2jSlo/0"), ("First Video", "8ueYlFT0G0c/0"), ("Second Video","kQkzHhFC0cc/0"), ("Third Video", "DEVVdu2jSlo/0")]
+        cell.cellDelegate = self
+        cell.tag = indexPath.row
         let (name, token) = videoData[indexPath.row]
         print ("http://img.youtube.com/vi/\(token).jpg")
         let imageURL = URL(string: "http://img.youtube.com/vi/\(token).jpg")
-        cell.setValues(picture1: imageURL!)
+        cell.setValues(picture1: imageURL!, row: indexPath.row)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print (indexPath.row)
+    }
+    
+    func didPressButton(_ tag: Int) {
+        print("I have pressed a button with a tag: \(tag)")
     }
 
     @IBAction func buyAction(_ sender: UIButton) {
