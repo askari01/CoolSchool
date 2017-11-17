12137e8c02b843c7b53f818d827fd68045156eb7
askari01 <farrukh.askari01@gmail.com>
2017-11-17 22:02:41 +0500

    added sentry and added intro video

---
+++ CoolSchool/LaunchScreenVC.swift
@@ -0,0 +1,35 @@
+//
+//  LaunchScreenVC.swift
+//  CoolSchool
+//
+//  Created by Syed Askari on 11/17/17.
+//  Copyright © 2017 Syed Askari. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
